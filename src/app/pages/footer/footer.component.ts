949ad236e8718ca988008874d6c6265ea41578dd
Gerardo Gabriel Caldas Díaz <u201512034@upc.edu.pe>
2025-10-03 11:58:26 -0500

    Modificación del footer y header

--- src/app/pages/footer/footer.component.ts
+++ src/app/pages/footer/footer.component.ts
@@ -1,16 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faFacebookF, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faInstagram, faWhatsapp, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterModule, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   faFacebook = faFacebookF;
   faInstagram = faInstagram;
   faWhatsapp = faWhatsapp;
+  faLinkedin = faLinkedinIn;
+
+  // Iconos de contacto
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faMapMarkerAlt = faMapMarkerAlt;
+
+  currentYear: number = 0;
+  isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  ngOnInit(): void {
+    this.currentYear = new Date().getFullYear();
+  }
+
+  scrollToTop(): void {
+    if (this.isBrowser) {
+      window.scroll({ 
+        top: 0, 
+        left: 0, 
+        behavior: 'smooth' 
+      });
+    }
+  }
 }
--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, HostListener, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,9 +9,50 @@ import { RouterModule } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   isMenuOpen: boolean = false;
+  isDesktop: boolean = true;
+  isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      this.checkWindowSize();
+    }
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    this.checkWindowSize();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(): void {
+    if (this.isBrowser) {
+      const header = document.querySelector('.header');
+      if (window.scrollY > 0) {
+        header?.classList.add('scrolled');
+      } else {
+        header?.classList.remove('scrolled');
+      }
+    }
+  }
+
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
+    if (this.isBrowser) {
+      document.body.style.overflow = this.isMenuOpen ? 'hidden' : '';
+    }
+  }
+
+  private checkWindowSize(): void {
+    this.isDesktop = window.innerWidth > 1024;
+    if (this.isDesktop && this.isMenuOpen) {
+      this.isMenuOpen = false;
+      document.body.style.overflow = '';
+    }
   }
 }
