b82e596c3c81d314031d1828850e8a283b8bdf1f
Gerardo Gabriel Caldas Díaz <u201512034@upc.edu.pe>
2025-10-03 12:37:48 -0500

    Mejora de quienes somos

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { 
+  faHome, faLeaf, faBolt, faHammer, faEye,
+  faShieldAlt, faThermometerHalf, faWallet, faTrophy
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
+  faHome = faHome;
+  faLeaf = faLeaf;
+  faBolt = faBolt;
+  faShieldAlt = faShieldAlt;
+  faThermometerHalf = faThermometerHalf;
+  faWallet = faWallet;
+  faTrophy = faTrophy;
 
+  faHammer = faHammer;
+  faEye = faEye;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.injectSolidIcons();
+  }
+
+  injectSolidIcons(): void {
+  }
 }
